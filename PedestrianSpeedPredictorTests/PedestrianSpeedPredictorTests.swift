8c2ed82e62c85e0a7f4a3226f69309ecb54a9347
“CazzaCoder” <caleb@storytellersweddingcollective.com>
2025-03-07 13:24:32 +1100

    Initial Commit

---
+++ PedestrianSpeedPredictorTests/PedestrianSpeedPredictorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PedestrianSpeedPredictorTests.swift
+//  PedestrianSpeedPredictorTests
+//
+//  Created by Caleb Nichols on 7/3/2025.
+//
+
+import Testing
+@testable import PedestrianSpeedPredictor
+
+struct PedestrianSpeedPredictorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
